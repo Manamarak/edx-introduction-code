a178b955b8628454aaa8122db23c61ce303f9c0d
Manamarak <32550461+Manamarak@users.noreply.github.com>
2018-03-20 23:27:09 +0000

    Update count vowels.py

--- my code/lecture 2/count vowels.py
+++ my code/lecture 2/count vowels.py
@@ -1,4 +1,5 @@
 def countVowels (s):
+    count = 0
     for character in s:
         if character == ('a'):
              count += 1
@@ -9,5 +10,15 @@ def countVowels (s):
         elif character == ('o'):
              count += 1
         elif character == ('u'):
+             count += 1
+        elif character == ('A'):
+             count += 1
+        elif character == ('E'):
+             count += 1
+        elif character == ('I'):
+             count += 1
+        elif character == ('O'):
+             count += 1
+        elif character == ('U'):
              count += 1
     print ('Number of vowels:', count)
